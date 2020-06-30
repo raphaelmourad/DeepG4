9e047e11f542472e1787466caa373e7c675972ee
rochevin <vincent.rocher.armd@gmail.com>
2020-06-30 15:54:34 +0200

    Add model and main function

--- R/DNAToNumerical.R
+++ R/DNAToNumerical.R
@@ -1,15 +1,28 @@
+#' Convert DNAStringSet object into one-hot encoding
+#'
+#' @param x DNAStringSet object
+#' @param tabv named vector list of numerical values which indicate the numerical value of a nucleotide.
+#' @param lower.case Set to \code{TRUE} if the DNA in your sequence is encoded in lower.case i.e. "acgt", default set to \code{FALSE}.
+#'
+#' @return An array of dimension \code{nrow(x),ncol(x),length(tabv)}
+#'
+#' @examples
+#' x <- DNAStringSet(c("ACGT"))
+#'
 DNAToNumerical <- function(x,tabv = c("N"=5,"T"=4,"G"=3,"C"=2,"A"=1),lower.case=F){
     if(lower.case){
         names(tabv) <- tolower(tabv)
     }
-    x <- as.matrix(x)
-    listMat=list()
-    for(i in tabv){
-        mat=matrix(0,nrow(x),ncol(x))
-        mat[x==names(i)]=1
-        listMat[[i]]=mat
+    x <- Biostrings::as.matrix(x)
+    listMat <- list()
+    for(i in 1:length(tabv)){
+        nuc_index <- tabv[[i]]
+        nuc_value <- names(tabv[i])
+        mat <- matrix(0,nrow(x),ncol(x))
+        mat[x==nuc_value] <- 1
+        listMat[[nuc_index]] <- mat
     }
-    arrayout=array(unlist(listMat), dim = c(nrow(listMat[[1]]), ncol(listMat[[1]]), length(listMat)))
+    arrayout <- array(unlist(listMat), dim = c(nrow(listMat[[1]]), ncol(listMat[[1]]), length(listMat)))
     return(arrayout)
 
 }
---
+++ R/DeepG4.R
@@ -0,0 +1,11 @@
+DeepG4 <- function(sequences = NULL,model = NULL,retrain=FALSE){
+    if (!requireNamespace("keras", quietly = TRUE)) {
+        stop("Package \"keras\" needed for this function to work. Please install it.",
+             call. = FALSE)
+    }
+    if (!requireNamespace("Biostrings", quietly = TRUE)) {
+        stop("Package \"Biostrings\" needed for this function to work. Please install it.",
+             call. = FALSE)
+    }
+
+}
