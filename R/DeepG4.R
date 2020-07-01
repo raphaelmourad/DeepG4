cdd9b3457d4fcb63a63dba3cc887fbd24405d23a
rochevin <vincent.rocher.armd@gmail.com>
2020-07-01 12:31:45 +0200

    Add model evaluation into DeepG4 function

--- R/DeepG4.R
+++ R/DeepG4.R
@@ -1,4 +1,4 @@
-DeepG4 <- function(X = NULL,Y=NULL,model = NULL,tabv = c("N"=5,"T"=4,"G"=3,"C"=2,"A"=1),lower.case=F,seq.size = 201){
+DeepG4 <- function(X = NULL,Y=NULL,model = NULL,tabv = c("N"=5,"T"=4,"G"=3,"C"=2,"A"=1),lower.case=F,seq.size = 201,treshold = 0.5){
     #Check if X is provided
     if (is.null(X)) {
         stop("X must be provided (see ?DeepG4 for accepted formats).",
@@ -61,6 +61,7 @@ DeepG4 <- function(X = NULL,Y=NULL,model = NULL,tabv = c("N"=5,"T"=4,"G"=3,"C"=2
     #Load model with keras (tensorflow must be installed as well)
     model <- keras::load_model_hdf5(model)
     res <- stats::predict(model,X)
+    return(res)
     #If Y is provided, instead of returning prediction, return accuracy / AUC
     if(!is.null(Y)){
         if(class(Y) != "numeric"){
@@ -71,8 +72,36 @@ DeepG4 <- function(X = NULL,Y=NULL,model = NULL,tabv = c("N"=5,"T"=4,"G"=3,"C"=2
             stop("Y must be a numeric vector of 1 and 0 values",
                  call. = FALSE)
         }
+        # Compute accuracy and AUC
+        if (!requireNamespace("ggplot2", quietly = TRUE)) {
+            stop("Package \"ggplot2\" needed for this function to work. Please install it.",
+                 call. = FALSE)
+        }
+        if (!requireNamespace("yardstick", quietly = TRUE)) {
+            stop("Package \"yardstick\" needed for this function to work. Please install it.",
+                 call. = FALSE)
+        }
+        prediction_table <- data.frame(
+            truth = as.factor(Y),
+            pred_prob = res[,1]
+        )
+        prediction_table$estimate <- as.factor(ifelse(prediction_table$pred_prob<treshold,0,1))
+        if(length(levels(prediction_table$truth))==1){
+            prediction_table$truth <- factor(prediction_table$truth,levels = c(1,0))
+        }
+        #Plot AUC
+        plot_ROC <- ggplot2::autoplot(yardstick::roc_curve(prediction_table,truth,pred_prob))
+        table_metrics <- yardstick::metrics(prediction_table,truth,estimate)
+        confusion_matrix <- as.data.frame(yardstick::conf_mat(prediction_table,truth, estimate)[[1]])
 
+        confusion_matrix <- ggplot2::ggplot(confusion_matrix,ggplot2::aes(Prediction, Truth, fill = Freq)) +
+            ggplot2::geom_tile(show.legend = FALSE) +
+            ggplot2::scale_fill_viridis_c() +
+            ggplot2::geom_text(ggplot2::aes(label = Freq), color = "white", alpha = 1, size = 8) +
+            ggplot2::labs(
+                title = "Confusion matrix"
+            ) + ggplot2::theme_minimal(base_size=18)
     }
 
-    return(res)
+    return(list(plot_ROC,confusion_matrix,table_metrics))
 }
