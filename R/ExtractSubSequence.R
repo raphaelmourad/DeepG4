fd28df163e04b06dc0eee1c2b76767e042aa2c41
rochevin <vincent.rocher.armd@gmail.com>
2020-07-16 10:41:32 +0200

    Add sub function to extract/generate sub sequences from a given sequence

---
+++ R/ExtractSubSequence.R
@@ -0,0 +1,20 @@
+#' Title
+#'
+#' @param x
+#' @param k
+#' @param seq.size
+#'
+#' @return
+#'
+#' @examples
+ExtractSubSequence <- function(x=NULL,k = 20,seq.size = 201){
+    extend <- ((seq.size-1)/2)
+    center <- seq(1,length(x),k)
+    start <- center - extend
+    end <- center + extend
+    Viewseq <- Biostrings::Views(x, start=start[start>0&end<length(x)], end=end[start>0&end<length(x)])
+    sequences <- Biostrings::DNAStringSet(Viewseq)
+
+    results <- cbind(as.data.frame(IRanges::ranges(Viewseq)),seq=as.character(Viewseq))
+    return(results)
+}
